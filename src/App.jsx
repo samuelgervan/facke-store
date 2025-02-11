16ad17919251825f51f736b1b31a3142db27c0b3
Samuel Gervan <gervan.cristiansamuel@iesconcepcion.edu.ar>
2025-02-10 21:13:50 -0300

    usando fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from react
 import './App.css'
+import {Products} from './components/Products'
+
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+  
+  const getProducts = async () =>{
+    let response= await fetch ("https://fakestoreapi.com/products")
+  let data = await response.json()
+  setProducts(data)
+  }
 
-  return (
+
+  useEffect (()=>{
+    getProducts()
+  },[])
+  if (products.length ==0){
+    return <>
+    <h1>cargando...</h1>
+    </>
+  }
+   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fake Store</h1>
+      <p>{products[0].tittle}</p>
+      <Products products={products[0]} />
+
+
+
+
+
+   
     </>
   )
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,13 @@
+export const Products =({products})=>{
+    return <>
+    <h1>{products.tittle}</h1>
+    <img src = {products.image}alt=""></img>
+    <h3>{products.price}</h3>
+    
+    </>
+    
+    
+    
+    
+    
+   }
